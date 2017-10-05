b05a63dce9ecdf06c8b78d76583c47fe5b4e4d2c
Andreajl <andreajl@stud.ntnu.no>
2017-10-05 17:11:20 +0200

    add/edit/remove events in calendar

--- personal_information_manager/src/components/Events/EventModal.js
+++ personal_information_manager/src/components/Events/EventModal.js
@@ -1,32 +1,47 @@
-import React, {Component} from 'react';
-import Modal from 'react-modal';
+import React, {Component} from 'react'
+import Modal from 'react-modal'
 
 class EventModal extends Component { 
   constructor(props){
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(e) {
-    const title = e
-    .target
-    .querySelector('input[id="title"]')
-    .value;
-
-    const event = {
-      title: title,
-      start: this.props.start,
-      end: this.props.end
+    super(props)
+    this.state = {
+      title: '',
+      start: '',
+      end: ''
     }
-
-    this.props.handleSubmit(event);
+    this.handleTitle = this.handleTitle.bind(this)
+    this.handleStart = this.handleStart.bind(this)
+    this.handleEnd = this.handleEnd.bind(this)
+    this.resetState = this.resetState.bind(this)
   }
 
-  makeTimeString(start, end) {
-    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+  /* Handles input in new tab text input field */
+  handleTitle(e) {
+    this.setState({ title: e.target.value })
+  }
 
-    var timeString = "";
+  handleStart(e) {
+    this.setState({ start: e.target.value })
+  }
+
+  handleEnd(e) {
+    this.setState({ end: e.target.value })
+  }
+
+  resetState() {
+    this.setState({
+      title: '',
+      start: '',
+      end: ''
+    })
+  }
+
+  // Todo fix
+  makeTimeString(start, end) {
+    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+    var months = ['January','February','March','April','May','June','July','August','September','October','November','December']
+
+    var timeString = ""
     timeString += days[start.getDay()] + ", "
     timeString += " " + months[start.getMonth()] + " " + start.getDay() + ", "
     timeString += start.getHours()+ ":" + start.getMinutes() + " - "
@@ -38,42 +53,89 @@ class EventModal extends Component {
       timeString += " " + months[end.getMonth()] + " " + end.getDay() + ", "
       timeString += end.getHours()+ ":" + end.getMinutes()
     }
-
     return timeString
   }
 
   render() {
-    const timeString = this.makeTimeString(this.props.start, this.props.end);
+    if (!this.props.modalEvent) return (<div> </div>)
+    //const timeString = this.makeTimeString(this.props.modalEvent.start, this.props.modalEvent.end)
 
     const modalStyles = {
+      overlay : {
+        zIndex                : 10
+      },
       content : {
         top                   : '50%',
         left                  : '50%',
         right                 : 'auto',
         bottom                : 'auto',
         marginRight           : '-50%',
-        transform             : 'translate(-50%, -50%)'
+        transform             : 'translate(-50%, -50%)',
       }
-    };
+    }
 
     return (
-        <div>
-          <Modal style={modalStyles} 
-            isOpen={this.props.open}
-            onRequestClose={this.props.handleClose}>
+      <div>
+        <Modal
+          style={modalStyles} 
+          isOpen={this.props.open}
+          onRequestClose={this.props.handleClose}>
 
-            <h2>Add event:</h2>
-            <form action="#" id="form" onSubmit={this.handleSubmit}>
-              <input placeholder="Untiteled event" id="title" className="form-control"/>
-              <hr />
-              <h5>When:</h5> {timeString}
-              <hr />
-              <input type="submit" value="submit" className="btn btn-success"/>
-            </form>
-          </Modal>
-        </div>
-    );
+          <div>
+            <h2> Event </h2>
+            <label> title: </label> {this.state.title}
+            <input className="form-control"  id="title" type="text"
+              placeholder={this.state.title} 
+              onChange={this.handleTitle}/>
+          </div>
+          <hr />
+          <div>
+            <label> start: </label>
+            {this.props.modalEvent.start.toString().substring(0, 15)}
+            <input className="form-control" id="start" type="time"
+              defaultValue={this.props.modalEvent.start.toString().substring(16,24)}
+              onChange={this.handleStart}
+            />
+          </div>
+          <div>
+            <label> end: </label> 
+            {this.props.modalEvent.end.toString().substring(0, 15)}
+            <input className="form-control" id="end" type="time"
+              defaultValue={this.props.modalEvent.end.toString().substring(16,24)}
+              onChange={this.handleEnd}
+            />
+          </div>
+          <hr />
+          <div>
+            <button 
+              className="btn btn-success"
+              onClick={() => {
+                this.props.handleSave(this.state)
+                this.resetState()
+              }}> Save
+            </button>
+            <button
+              className="btn btn-danger"
+              onClick={() => {
+                this.props.handleRemove(this.props.modalEvent)
+                this.resetState()
+              }}> Delete 
+            </button>
+            <button 
+              className="btn btn-warning"
+              onClick={() => {
+                this.props.handleClose()
+                this.resetState()
+              }}> Close
+            </button>
+          </div>
+          <div>
+            {this.props.error}
+          </div>
+        </Modal>
+      </div>
+    )
   }
 }
 
-export default EventModal; 
+export default EventModal 
