bd1b22eaade30edf890bc45c5ad9872656542b72
andreajl <andreajl@stud.ntnu.no>
2017-10-06 17:35:56 +0200

    cut down some of the code, easier to read and modify. (#31)

--- personal_information_manager/src/components/Notifications/Notifications.js
+++ personal_information_manager/src/components/Notifications/Notifications.js
@@ -2,27 +2,12 @@ import React, { Component } from "react";
 import { loadFromLocalStorage} from '../../localStorage'
 
 export function getAppoitments(){
-  var calenderAppoitments = loadFromLocalStorage("events");
-  if(!calenderAppoitments){
-    return [];
-  }
-  var appoitments = [];
-  for (var i = 0; i < Object.keys(calenderAppoitments).length; i++) {
-      var startDate = calenderAppoitments[i].start;
-      var date = startDate.slice(0, 4)+" "+startDate.slice(5, 7)+" "+ startDate.slice(8, 10)
-      var thisDate = new Date();
-      thisDate.setDate(thisDate.getDate()-1);
-      thisDate = thisDate.toDateString();
-      date = new Date(date).toDateString();
-      if(thisDate === date){
-        appoitments.push(calenderAppoitments[i].title)
-      }
-   }
-  return appoitments;
+  const appoitments = loadFromLocalStorage('events', [])
+  const today = new Date().toDateString()
+  return appoitments.filter(d => new Date(d.start).toDateString() === today)
 }
 
 class Notifications extends Component{
-
   constructor(props) {
     super(props);
     this.state = {
@@ -32,11 +17,9 @@ class Notifications extends Component{
   }
 
   handleChange() {
-    this.setState({ appoitments: getAppoitments()});
-  }
-
-  getDay(){
-    return (new Date().toDateString());
+    this.setState({ 
+      appoitments: getAppoitments()
+    });
   }
 
   render(){
@@ -47,11 +30,11 @@ class Notifications extends Component{
           <div className="row">
             <div className="col-lg-8 mx-auto">
               <h2>Notfications</h2>
-              <p className="lead">Notifications for: <kbd>{this.getDay()}</kbd></p>
+              <p className="lead">Notifications for: <kbd>{new Date().toDateString()}</kbd></p>
               <ul>
                 {/*TODO: update list when new appoitments are a added*/}
                 {/* Generates a "ul" list from appoitments with unique id's */}
-                {this.state.appoitments.map((appoitment, i) => <li key={i}><span className="badge badge-danger" id="liBadge">{i+1} </span>{appoitment}</li>)}
+                {this.state.appoitments.map((appoitment, i) => <li key={i}><span className="badge badge-danger" id="liBadge">{i+1} </span>{appoitment.title}</li>)}
               </ul>
             </div>
           </div>
