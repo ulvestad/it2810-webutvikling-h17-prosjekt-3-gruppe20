b2fc3408fca7ebfe8cecf3eff7725de32e88eb66
Ole-Martin Skagevang <hero3@hotmail.com>
2017-10-12 13:32:23 +0200

    create components (#50)

--- personal_information_manager/native/components/Todo.js
+++ personal_information_manager/native/components/Todo.js
@@ -10,14 +10,12 @@ const styles = StyleSheet.create({
   },
 });
 
-const App = () => {
+const Todo () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to hallaksnfpowndocworking on your app!</Text>
-      <Text>Changes you make will automatically reload.</Text>
-      <Text>Shake your phone to open the developer menu.</Text>
+      <Text>TODO</Text>
     </View>
   );
 };
 
-export default App;
+export default Todo;
