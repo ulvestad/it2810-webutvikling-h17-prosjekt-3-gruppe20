0c7801abc58c52f227280c56d0b4ddef5ba8d4a9
Simon Olsen <smholsen@stud.ntnu.no>
2017-10-17 23:07:07 +0200

    More events tests

--- personal_information_manager/react/src/components/Events/Events.test.js
+++ personal_information_manager/react/src/components/Events/Events.test.js
@@ -24,12 +24,41 @@ it('starts with empty end', () => {
     assert.equal(domref.state.end, '', 'initial end is empty');
 });
 
-/*
-it('calls handler when form is submitted', () => {
-    const handler = sinon.spy();
-    const wrapper = shallow(<EventModal/>);
-    wrapper.find('button').simulate('click');
-    expect(handler.args[0][0]).to.equal(2);
-    expect(handler).to.have.property('callCount', 1);
+
+it('updates state with title', () => {
+  const wrapper = shallow(<EventModal/>);
+  let value = 'testTitle';
+  let target = {value};
+  let event = {target};
+  wrapper.instance().handleTitle(event);
+
+  expect(wrapper.state()).to.have.property('title', value);
 });
-*/
+
+it('updates state with startDate', () => {
+    const wrapper = shallow(<EventModal/>);
+    let value = '10.15.2017';
+    let target = {value};
+    let event = {target};
+    wrapper.instance().handleStart(event);
+
+    expect(wrapper.state()).to.have.property('start', value);
+});
+
+it('updates state with endDate', () => {
+    const wrapper = shallow(<EventModal/>);
+    let value = '10.15.2018';
+    let target = {value};
+    let event = {target};
+    wrapper.instance().handleEnd(event);
+
+    expect(wrapper.state()).to.have.property('end', value);
+});
+
+it('formats date correctly', () => {
+    const wrapper = shallow(<EventModal/>);
+
+    expect(wrapper.instance().formatDateToDay(
+        new Date('10.10.2010')
+    )).to.equal('Sun Oct 10 2010');
+});
