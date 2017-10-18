7a1ac5a29efa55b9e1f10a29d19a605a0ddf586b
Kristian Aurlien <kristian@aurlien.net>
2017-10-18 16:06:19 +0200

    add change/delete of notes

--- personal_information_manager/native/components/AddNote.js
+++ personal_information_manager/native/components/AddNote.js
@@ -1,56 +1,73 @@
 import React, {Component} from 'react';
-import {StyleSheet, View, Text, TextInput, Button, ScrollView} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  ScrollView,
+  TouchableHighlight
+} from 'react-native';
 import PropTypes from 'prop-types';
 import {storeItem, getNotes} from '../asyncStorage';
 
 class AddNote extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    this.state = {text: ""};
+    const note = this.props.note;
+
+    this.state = {
+      text: note ? note.text : '',
+      id: note ? note.id : new Date().getTime()
+    };
 
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   handleSubmit(e) {
-    console.log("Create new note!");
-
-    const note = {
-      id: new Date().getTime(),
-      text: this.state.text
-    }
-
+    const note = this.state;
     this.props.onSubmit(note);
+  }
 
+  handleDelete(e) {
+    console.log('delete note:' + this.state.id);
+    this.props.onDelete(this.state.id);
   }
 
   render() {
     const styles = {
       container: {
-        flex: 1,
+        flex: 1
       },
       inputContainer: {
-        flex: 1,
+        flex: 1
       },
       inputField: {
-        fontSize: 20,  
-      },
+        fontSize: 20
+      }
     };
 
     return (
       <View style={styles.container}>
-        <View style={{marginBottom: 10}}>
-            <Button title="Submit" onPress={this.handleSubmit}/>
+        <View
+          style={{
+          marginBottom: 10,
+          justifyContent: 'space-between'
+        }}
+          flexDirection='row'>
+          <Button title="Delete" onPress={this.handleDelete}/>
+          <Button title="Submit" onPress={this.handleSubmit}/>
         </View>
 
-        <View style={styles.inputContainer}>
+        <View style={styles.inputContainer} keyboardShouldResistTap={true}>
           <TextInput
             onChangeText={(text) => this.setState({text})}
             value={this.state.text}
             autoFocus={true}
-            multiline = {true}
-            style={styles.inputField}
-          />
+            multiline={true}
+            style={styles.inputField}/>
         </View>
 
       </View>
