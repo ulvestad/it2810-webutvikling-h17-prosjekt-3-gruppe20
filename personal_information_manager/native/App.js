507af4f2b93b34eefb8919678f747afb00df7b2e
Ole-Martin Skagevang <hero3@hotmail.com>
2017-10-18 16:04:08 +0200

    fixed header (#60)

--- personal_information_manager/native/App.js
+++ personal_information_manager/native/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleSheet, Text, View, ScrollView, Platform} from 'react-native';
-import {TabNavigator} from 'react-navigation';
+import {TabNavigator, StackNavigator} from 'react-navigation';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import LogoBar from './components/LogoBar';
 import Todo from './components/Todo';
@@ -20,7 +20,6 @@ const styles = StyleSheet.create({
 /*Notes view with corresponding components*/
 const NotesScreen = ({ navigation }) => (
   <View style={{flex: 1}}>
-    <LogoBar/>
     <ScrollView>
       <View style={styles.container}>
         <Todo/>
@@ -32,7 +31,6 @@ const NotesScreen = ({ navigation }) => (
 /*Todo view with corresponding components*/
 const TodoScreen = ({ navigation }) => (
   <View style={{flex: 1}}>
-    <LogoBar/>
     <ScrollView>
       <View style={styles.container}>
         <Todo/>
@@ -44,7 +42,6 @@ const TodoScreen = ({ navigation }) => (
 /*Calendar view with corresponding components*/
 const EventsScreen = ({ navigation }) => (
   <View style={{flex: 1}}>
-    <LogoBar/>
     <ScrollView>
       <View style={styles.container}>
         <Events/>
@@ -55,6 +52,7 @@ const EventsScreen = ({ navigation }) => (
 
 /*Navigation options for the "Todo" tab*/
 TodoScreen.navigationOptions = {
+  header: <LogoBar/>,
   tabBarLabel: 'Todo',
   tabBarIcon: ({ tintColor, focused }) => (
     <Ionicons
@@ -67,6 +65,7 @@ TodoScreen.navigationOptions = {
 
 /*Navigation options for the "Notes" tab*/
 NotesScreen.navigationOptions = {
+  header: <LogoBar/>,
   tabBarLabel: 'Notes',
   tabBarIcon: ({ tintColor, focused }) => (
     <Ionicons
@@ -79,6 +78,7 @@ NotesScreen.navigationOptions = {
 
 /*Navigation options for the "Calendar" tab*/
 EventsScreen.navigationOptions = {
+  header: <LogoBar/>,
   tabBarLabel: 'Calendar',
   tabBarIcon: ({ tintColor, focused }) => (
     <Ionicons
@@ -90,29 +90,35 @@ EventsScreen.navigationOptions = {
 };
 
 /*Routing using react-navigation*/
-const App = TabNavigator(
-  {
-    Calendar: {
-      screen: EventsScreen,
-      path: 'calendar',
-    },
-    Todo: {
-      screen: TodoScreen,
-      path: 'todo',
-    },
-    Notes: {
-      screen: NotesScreen,
-      path: 'notes',
-    }
-  },
-  {
-    tabBarPosition: 'bottom',
-    swipeEnabled: true,
-    tabBarOptions: {
-      activeTintColor: Platform.OS === 'ios' ? '#0092ff' : '#fff',
-      style: {backgroundColor: '#343A40'}
-    }
+const App =  StackNavigator({
+   MyTab: {
+     screen: TabNavigator(
+       {
+         Calendar: {
+           screen: EventsScreen,
+           path: 'calendar',
+         },
+         Todo: {
+           screen: TodoScreen,
+           path: 'todo',
+         },
+         Notes: {
+           screen: NotesScreen,
+           path: 'notes',
+         }
+       },
+       {
+         tabBarPosition: 'bottom',
+         swipeEnabled: true,
+         animationEnabled: true,
+         tabBarOptions: {
+           activeTintColor: Platform.OS === 'ios' ? '#0092ff' : '#fff',
+           style: {backgroundColor: '#343A40'}
+         }
+       }
+     ),
+     navigationOptions: { title: 'Header title' }
   }
-);
+})
 
 export default App;
