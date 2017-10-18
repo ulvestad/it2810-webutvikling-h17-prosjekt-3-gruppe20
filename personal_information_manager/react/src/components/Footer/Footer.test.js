5c18bff3c0e608f3920db023122b06e74a100763
Kristoffer <kra1992@hotmail.com>
2017-10-18 20:48:45 +0200

    Create snapshot test for Footer component

--- personal_information_manager/react/src/components/Footer/Footer.test.js
+++ personal_information_manager/react/src/components/Footer/Footer.test.js
@@ -3,6 +3,13 @@ import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import Footer from './Footer';
 
+const jestExpect = global.expect;
+
 it('Footer renders without crashing', () => {
   shallow(<Footer />);
+});
+
+it('renders correctly', () => {
+  const tree = renderer.create(<Footer />).toJSON();
+  jestExpect(tree).toMatchSnapshot();
 });
