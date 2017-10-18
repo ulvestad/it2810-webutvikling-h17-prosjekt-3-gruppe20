353a2d921006395cdf5f598ddf036a7e0472dfb6
Kristoffer <kra1992@hotmail.com>
2017-10-18 21:03:15 +0200

    Create snapshot test for App component

--- personal_information_manager/react/src/components/App/App.test.js
+++ personal_information_manager/react/src/components/App/App.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import sinon from 'sinon';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
+import {BrowserRouter} from 'react-router-dom'
 
 import App from './App';
 
+const jestExpect = global.expect;
+
 it('renders without crashing', () => {
   shallow(<App />);
+});
+
+it('renders correctly', () => {
+  const appWrappedInRouter = (
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+  );
+  const tree = renderer.create(appWrappedInRouter).toJSON();
+  jestExpect(tree).toMatchSnapshot();
 });
