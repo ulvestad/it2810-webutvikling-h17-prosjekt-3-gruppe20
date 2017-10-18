e51b3f897babbeadb063b2df78f894c35ab74bc2
Kristoffer <kra1992@hotmail.com>
2017-10-18 20:47:32 +0200

    Create snapshot test for Header component

--- personal_information_manager/react/src/components/Header/Header.test.js
+++ personal_information_manager/react/src/components/Header/Header.test.js
@@ -3,6 +3,13 @@ import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import Header from './Header';
 
+const jestExpect = global.expect;
+
 it('Header renders without crashing', () => {
   shallow(<Header />);
+});
+
+it('renders correctly', () => {
+  const tree = renderer.create(<Header />).toJSON();
+  jestExpect(tree).toMatchSnapshot();
 });
