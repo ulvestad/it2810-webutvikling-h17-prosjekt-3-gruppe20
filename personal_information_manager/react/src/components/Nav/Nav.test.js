30ffe698537ffb9e56ad65dd7cabe342ffa4a310
Kristoffer <kra1992@hotmail.com>
2017-10-18 20:46:23 +0200

    Create snapshot test for Nav component

--- personal_information_manager/react/src/components/Nav/Nav.test.js
+++ personal_information_manager/react/src/components/Nav/Nav.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import sinon from 'sinon';
+import renderer from 'react-test-renderer';
 import { expect } from 'chai';
 import { shallow, mount } from 'enzyme';
 import { Link } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom'
 
 import Nav from './Nav';
+
+const jestExpect = global.expect;
 
 beforeEach(() => {
   localStorage.getItem.reset();
@@ -14,6 +18,16 @@ beforeEach(() => {
 
 it('renders without crashing', () => {
   shallow(<Nav />);
+});
+
+it('renders correctly', () => {
+  const navWrappedInRouter = (
+      <BrowserRouter>
+        <Nav/>
+      </BrowserRouter>
+  );
+  const tree = renderer.create(navWrappedInRouter).toJSON();
+  jestExpect(tree).toMatchSnapshot();
 });
 
 it('runs updateNotificationBadge when clicked', () => {
